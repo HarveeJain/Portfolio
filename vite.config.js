9c99b8e7814bf27c09b6186781fa3a364ba3b1f5
HarveeJain <harveej2006@gmail.com>
2026-05-27 19:17:19 +0530

    Fix import syntax and update base path in config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// IMPORTANT: replace "YOUR_REPO_NAME"
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/YOUR_REPO_NAME/",
+});
